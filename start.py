7aa6a870a08b0354b6b89b914c47c5d4447306d9
Dorji <daoerji.si.2030@esm.edu.mn>
2023-01-14 11:16:40 +0800

    docs

--- start.py
+++ start.py
@@ -3,15 +3,15 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from time import sleep
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
+driver = webdriver.Firefox() # gets the webdriver from Firefox
+driver.get("http://www.python.org") # goes to the given url
+assert "Python" in driver.title # checks if the title of the given url cotains the word "Python"
+elem = driver.find_element(By.NAME, "q") # finds the elem(element) "q" inside name="q", name="q is inside the html code of the website"
+elem.clear() # clearing the things in the searchbox
+sleep(2) # wait for 2 seconds to show the user what is happening
+elem.send_keys("pycon") # types the word "pycon" inside the search box
 sleep(2)
-elem.send_keys("pycon")
-sleep(2)
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+elem.send_keys(Keys.RETURN) # clicks the enter key(return key on macos)
+assert "No results found." not in driver.page_source # checks if the text "No results found." was in the page_source of the driver
 sleep(20)
-driver.close()
+driver.close() # closes the driver
