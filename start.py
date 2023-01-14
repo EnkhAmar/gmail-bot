d4c449ab98bef036dbf66df78875e565d9954f13
EnkhAmar <enkhamar1220@gmail.com>
2023-01-14 10:57:03 +0800

    debug with sleep

--- start.py
+++ start.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from time import sleep
 
 driver = webdriver.Firefox()
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 elem = driver.find_element(By.NAME, "q")
 elem.clear()
+sleep(2)
 elem.send_keys("pycon")
+sleep(2)
 elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
+sleep(20)
 driver.close()
