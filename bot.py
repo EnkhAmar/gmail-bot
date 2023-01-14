aee834c3945e554a54b7b6f9b12793614dce3076
EnkhAmar <enkhamar1220@gmail.com>
2023-01-14 11:32:18 +0800

    tasks

---
+++ bot.py
@@ -0,0 +1,21 @@
+"""
+TODO: Creating a new account
+1. Go to https://gmail.com/
+2. Click on the "create account" drop down list and choose "for my personal use"
+3. Fill the create new account form and click "next" button
+4. On the next page, check if the "phone number" is optional. If the phone number is optional goto 4.1, else 4.2.
+4.1. Fill the form and click "next" button
+4.2. Leave this later
+5. Agree the policy
+"""
+
+"""
+TODO: Login using existing account
+1. Go to https://gmail.com/
+2. Click on the "create account" drop down list and choose "for my personal use"
+3. Fill the create new account form and click "next" button
+4. On the next page, check if the "phone number" is optional. If the phone number is optional goto 4.1, else 4.2.
+4.1. Fill the form and click "next" button
+4.2. Leave this later
+5. Agree the policy
+"""
